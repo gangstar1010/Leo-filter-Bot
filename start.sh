4c6b31a88ba4bc0504ff18c6be1be6fb7bfc4fe5
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2023-08-29 15:59:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/1238muj/THALAPATHY-FILTER-BOT.git /THALAPATHY-FILTER-BOT 
+  git clone https://github.com/gangstar1010/Leo-filter-Bot.git /Leo-filter-Bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Leo-filter-Bot 
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /Leo-filter-Bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
